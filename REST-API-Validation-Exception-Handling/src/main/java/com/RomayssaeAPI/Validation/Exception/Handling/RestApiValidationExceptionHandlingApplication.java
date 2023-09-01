5fb6f435f68971a2c4649dc1f0b7914a8cef64fa
Romayssae21 <romayssae.21.chan@gmail.com>
2023-09-01 20:07:34 +0100

    Create a UserRequest DTO class and a UserRepository interface

---
+++ REST-API-Validation-Exception-Handling/src/main/java/com/RomayssaeAPI/Validation/Exception/Handling/RestApiValidationExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.RomayssaeAPI.Validation.Exception.Handling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiValidationExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiValidationExceptionHandlingApplication.class, args);
+	}
+
+}
---
+++ REST-API-Validation-Exception-Handling/src/test/java/com/RomayssaeAPI/Validation/Exception/Handling/RestApiValidationExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RomayssaeAPI.Validation.Exception.Handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiValidationExceptionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
