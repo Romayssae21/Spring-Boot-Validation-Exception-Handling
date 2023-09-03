18dbf9f0f292d58b833ad058b569a1fadd1fc092
Romayssae21 <romayssae.21.chan@gmail.com>
2023-09-03 07:29:49 +0100

    Validating incoming request by using bean validation concept and adding some constraints

---
+++ REST-API-Validation-Exception-Handling/src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE "USER"(
+    userId INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    mobile VARCHAR(255),
+    gender VARCHAR(1),
+    age INT,
+    nationality VARCHAR(255)
+);
+
+
